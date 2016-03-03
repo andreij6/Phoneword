7c8bbc65527c82d54af61529ce8374692e69e9de
andreij6 <andreij6@gmail.com>
2016-03-02 21:05:19 -0500

    Phoneword Xamarin (Hello World)
    Built IOS and Android versions of the tutorial.  the app uses common code and abstracts away the Dialer functionality that have specfic implementations in the two platforms.
    
    Completed Quick Start

---
+++ PhoneWordApp/PhoneWordApp.iOS/PhoneDialer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Foundation;
+using UIKit;
+using PhoneWordApp.iOS;
+using Xamarin.Forms;
+using PhoneWordApp;
+
+[assembly: Dependency(typeof(PhoneDialer))]
+namespace PhoneWordApp.iOS
+{
+	public class PhoneDialer : IDialer
+	{
+		public bool Dial(string number)
+		{
+			return UIApplication.SharedApplication.OpenUrl(
+			    new NSUrl("tel:" + number));
+		}
+	}
+}
